92b0eb244a5106bae3f6dac14a021d0bf10d666e
SquallStrifeX <squallstrifex@gmail.com>
2023-11-29 18:13:55 +0100

    end bonus 2

--- Bonus 2/js/script.js
+++ Bonus 2/js/script.js
@@ -3,12 +3,37 @@ let buttonStart = document.getElementById('button_start');
 
 buttonStart.addEventListener("click", function(){
  containerPadre.innerHTML = " "
-    for(i=0; i<100; i++){
-    creazioneDiUnQuadrato();
+ let input_difficulty = document.getElementById('difficolta');
+ let difficulty = parseInt(input_difficulty.value);
+ console.log(difficulty)
+
+
+    switch (difficulty){
+      case 1:
+        containerPadre.innerHTML = " "
+        for(i=0; i<100; i++){
+          creazioneDiUnQuadrato();
+            }
+            break
+      case 2:
+        containerPadre.innerHTML = " "
+        for(i=0; i<81; i++){
+          creazioneDiUnQuadrato();
+            }
+            containerPadre.classList.add("container_81")
+
+            break
+      case 3:
+         containerPadre.innerHTML = " "
+         for(i=0; i<49; i++){
+           creazioneDiUnQuadrato();
+            }
+            containerPadre.classList.add("container_49")
+             break     
+
+    }
     
    
-   
-      }
 })
 
 let containerPadre = document.getElementById('container_padre')
@@ -19,7 +44,8 @@ let containerPadre = document.getElementById('container_padre')
     containerPadre.appendChild(nuovoQuadrato);
     nuovoQuadrato.textContent = (i+1);
     nuovoQuadrato.addEventListener("click", function onClick(event){
-    nuovoQuadrato.classList.toggle("bg-warning")
+    nuovoQuadrato.classList.toggle("bg-warning");
+    console.log(nuovoQuadrato.textContent)
 
     })
 }
